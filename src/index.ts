f744ce15d47f4298d0832bcdcaa1a090e369435f
vitorelourenco <vitor.registros@lourencos.net>
2022-04-11 09:43:03 -0300

    .

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { BrowserTracing } from "@sentry/tracing";
+import { BrowserClient, Hub } from "@sentry/browser";
+
+const client = new BrowserClient({
+  dsn: "https://a842cbfdb2434556a09ea4efaede2b58@o1190326.ingest.sentry.io/6311376",
+  tracesSampleRate: 1.0,
+  integrations: [new BrowserTracing()],
+  beforeSend(event, hint) {
+    console.log('hint', hint);
+    return event;
+  },
+});
+const hub = new Hub(client);
+
+try {
+  // @ts-ignore
+  abacaxi();
+} catch (err) {
+  console.log("eyyyy");
+  hub.captureException(err);
+}
---
+++ webpack.config.cjs
@@ -0,0 +1,53 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  plugins: [new webpack.SourceMapDevToolPlugin({
+    filename: '[name].js.map',
+    exclude: [/(node_modules|bower_components)/]
+  })],
+  optimization: {
+    usedExports: true,
+  },
+  mode: "production",
+  target: "es5",
+  entry: {
+    index: "./index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    library: {
+      type: "window",
+      name: "[name]",
+    },
+    chunkFormat: "array-push",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.m?js$|\.tsx?$)/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/typescript",
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: "usage",
+                  targets: "defaults",
+                  corejs: 3,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
