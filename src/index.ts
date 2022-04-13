0e0d3a3ead193ff746a25e52ffb1637eb370ca36
vitorelourenco <vitor.registros@lourencos.net>
2022-04-13 11:43:47 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
-import { BrowserClient, Hub } from "@sentry/browser";
 
-const client = new BrowserClient({
+const client = new Sentry.BrowserClient({
   dsn: "https://d1648f12a72140d39bb0b0ebc11dbf15@o1199623.ingest.sentry.io/6323406",
   tracesSampleRate: 1.0,
   integrations: (defaultIntegrations) => [...defaultIntegrations, new BrowserTracing()],
@@ -11,7 +11,7 @@ const client = new BrowserClient({
     return event;
   },
 });
-const hub = new Hub(client);
+const hub = new Sentry.Hub(client);
 
 try {
   // @ts-ignore
